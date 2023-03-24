bc78929e5c4e631161305f9270d7a238dbd69eab
Levonte Larry <you@example.com>
2023-03-24 19:00:32 -0400

    packages deals prices

--- src/pages/Prices.js
+++ src/pages/Prices.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import gd from '../assets/gd.png'
+import gd from '../assets/gd.png';
+import price from '../assets/price.png';
 
 function Prices(props) {
   return (
     <div className='price'>
         <img 
             src={gd} 
+            alt='mori'
+            className="pimg"
+            />
+        <img 
+            src={price} 
             alt='mori'
             className="pimg"
           />
