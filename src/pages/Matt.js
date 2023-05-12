a879274c3cf845d586128b61781477092778a5e9
Levonte Larry <you@example.com>
2023-05-11 20:08:22 -0400

    background athletes

--- src/pages/Matt.js
+++ src/pages/Matt.js
@@ -16,21 +16,21 @@ function Matt(props) {
           />
         <h1> MATT </h1>
         <h2> Click Any Image To Watch Workouts</h2>
-    <NavLink to='/kris'>
+    <NavLink to='https://youtu.be/soPquQQOE1Y'>
     <img 
             src={matt3} 
             alt='matt workout video'
             className="videos"
           />
     </NavLink>
-    <NavLink to='/kris'>
+    <NavLink to='https://youtu.be/v-UW_4SJp6w'>
         <img 
                 src={matt2} 
                 alt='matt workout video'
                 className="videos"
               />
     </NavLink>
-    <NavLink to='/kris'>
+    <NavLink to='https://youtu.be/QqrSWbXn1A8'>
         <img 
                 src={matt1} 
                 alt='matt workout video'
